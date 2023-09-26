9a4546c659b2790080c5799837c91ead12e1226e
serena <142355033+slnicole@users.noreply.github.com>
2023-09-26 23:03:44 +0000

    Update 5 files

--- src/home/components/MainSection.tsx
+++ src/home/components/MainSection.tsx
@@ -7,7 +7,7 @@ import {
 } from "@mui/material";
 
 const MainSection = () => {
-  const title = "Vos queres crecer.\n Nosotros te queremos ayudar";
+  const title = "Vos no queres crecer.\n Nosotros te queremos ayudar";
   const subtitle =
     "Crea una web con plantillas impecables, fácil edición y sin codificación.";
   const sm = useMediaQuery("min-width(600px)");
--- src/theme/Theme.tsx
+++ src/theme/Theme.tsx
@@ -4,7 +4,7 @@ const options: ThemeOptions = {
   palette: {
     primary: {
       light: "#757ce8",
-      main: "#3f50b5",
+      main: "#625d65",
       dark: "#002884",
       contrastText: "#fff",
     },
